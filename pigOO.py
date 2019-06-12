993fb90e28831ba3f2bcc9f3da9003c276eb8830
Daniel E Calderon <Daniel.Calderon06@gmail.com>
2019-06-12 12:59:04 -0400

    just setting up ideas

---
+++ pigOO.py
@@ -0,0 +1,43 @@
+import random
+
+class Player:
+    __init__( self,  )
+
+
+def ():
+    pass
+
+
+class Dice:
+   __init__ (self, random)
+
+min = 1
+max = 6
+
+roll_again = "yes"
+
+while roll_again == "yes" or roll_again == "y":
+    print "Rolling the dices..."
+    print "The values are...."
+    print random.randint(min, max)
+    print random.randint(min, max)
+
+    roll_again = raw_input("Roll the dices again?") 
+
+
+def ():
+    pass
+
+class Score:()
+__init__ (self, )
+
+def ():
+    pass
+
+class Turn:
+__init__(self,)
+
+
+player_one_score = []
+computer_player = []
+
